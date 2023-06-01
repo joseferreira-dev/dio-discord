de1cf27d39be8450c72fdaea44b92ed0cbcc2969
José Ferreira <joseguedes.dev@gmail.com>
2023-06-01 10:10:41 -0300

    Make the design responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// MENU MOBILE SCRIPTS
+
+const menuMobile = document.querySelector('.header__nav-mobile');
+const menuIcon = document.querySelector('.header__icon img');
+
+function activeMenu() {
+  menuMobile.classList.toggle('hidden');
+  menuIcon.setAttribute('src', './assets/img/menu-icon-open.svg');
+
+  if(menuMobile.classList.contains('hidden')) {
+    menuIcon.setAttribute('src', './assets/img/menu-icon-close.svg');
+  }
+}
+
+menuIcon.addEventListener('click', activeMenu);
+
+// FAQ SECTION SCRIPTS
+
 const faqQuestions = document.querySelectorAll('.faq__questions li');
 const faqAnswers = document.querySelectorAll('.faq__answers li');
 
